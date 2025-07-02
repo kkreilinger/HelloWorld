ef8dd466db2696e78bf84afc1b16f1277882ee26
Kurt Kreilinger <k.kreilinger@gmail.com>
2025-07-02 17:45:28 +0200

    Fix header message

--- HelloWorldApp/src/main/java/helloworldapp/HelloWorldApp.java
+++ HelloWorldApp/src/main/java/helloworldapp/HelloWorldApp.java
@@ -3,6 +3,6 @@ package helloworldapp;
 public class HelloWorldApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello!");
     }
 }
